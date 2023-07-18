0c6eb6d82bf0a07d987172686b2b9e55275c1f2a
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-07-18 09:06:46 +0900

    [FIX] Operation을 class에서 protocol로 변경

--- calculator/calculator.swift
+++ calculator/calculator.swift
@@ -1,30 +1,28 @@
 import Foundation
 
-class Operation {
-    func operate(_ a: Double, _ b: Double) -> Double {
-        return 0
-    }
+protocol Operation {
+    func operate(_ a: Double, _ b: Double) -> Double
 }
 class AddOperation: Operation {
-    override func operate(_ a: Double, _ b: Double) -> Double {
+    func operate(_ a: Double, _ b: Double) -> Double {
         return a + b
     }
 }
 
 class SubstractOperation: Operation {
-    override func operate(_ a: Double, _ b: Double) -> Double {
+    func operate(_ a: Double, _ b: Double) -> Double {
         return a - b
     }
 }
 
 class MultiflyOperation: Operation {
-    override func operate(_ a: Double, _ b: Double) -> Double {
+    func operate(_ a: Double, _ b: Double) -> Double {
         return a * b
     }
 }
 
 class DivideOperation: Operation {
-    override func operate(_ a: Double, _ b: Double) -> Double {
+    func operate(_ a: Double, _ b: Double) -> Double {
         return a / b
     }
 }
